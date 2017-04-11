1e26fe5c46044023609c467494a2dbfd2aecec8c
WpSmile <179292126@qq.com>
2017-04-11 23:21:42 +0800

    添加新手指导、设置、即将揭晓、中奖纪录页面及其布局，添加相应资源文件

---
+++ app/src/main/java/com/example/administrator/fastshop/view/activity/BeginerGuideActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.fastshop.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.fastshop.R;
+
+public class BeginerGuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beginer_guide);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/fastshop/view/activity/WinningRecordActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.fastshop.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.fastshop.R;
+
+public class WinningRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_winning_record);
+    }
+}
