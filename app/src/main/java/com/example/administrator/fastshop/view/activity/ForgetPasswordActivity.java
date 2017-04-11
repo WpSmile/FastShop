79d7178a5a65b0917ed66d771ab3ccf2d32087d6
WpSmile <179292126@qq.com>
2017-04-11 10:07:20 +0800

    完成手机注册页面和找回密码页面的UI布局并添加相应资源文件

---
+++ app/src/main/java/com/example/administrator/fastshop/view/activity/ForgetPasswordActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.fastshop.view.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
